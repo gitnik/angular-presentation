92e5c0fde0171c440f6446daefad7d7451eae834
Nik Karbaum <nik.karbaum@gmail.com>
2014-01-13 15:57:15 +0100

    wrong function

--- src/app/factories.js
+++ src/app/factories.js
@@ -24,7 +24,7 @@ factory('CurrencyExchangeData', ['$http', '$q', function($http, $q) {
 
         var deffered = $q.defer();
 
-        $http.json('app/exchangeData.json').success(function(data) {
+        $http.get('app/exchangeData.json').success(function(data) {
             deffered.resolve(JSON.parse("'" + data + "'"));
         });
 
