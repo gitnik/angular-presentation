e1e7a48389e1927f3aa75d766d68d013e50582f1
Nik Karbaum <nik.karbaum@gmail.com>
2014-01-13 16:02:28 +0100

    we dont need JSOn.parse

--- src/app/factories.js
+++ src/app/factories.js
@@ -25,7 +25,7 @@ factory('CurrencyExchangeData', ['$http', '$q', function($http, $q) {
         var deffered = $q.defer();
 
         $http.get('app/exchangeData.json').success(function(data) {
-            deffered.resolve(JSON.parse("\'" + data + "\'"));
+            deffered.resolve(data);
         });
 
         return deffered.promise;
