e51ceb253bf8135c414e309470ab3e491112a644
Nik Karbaum <nik.karbaum@gmail.com>
2014-01-13 12:57:39 +0100

    add new task on click

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -6,7 +6,8 @@ controller('ToDoController', ['$scope', function($scope) {
     $scope.tasks = ['Task1', "Task2", "Task3"];
 
     $scope.addNewTask = function() {
-        $scope.tasks.push("");
+        $scope.tasks.push(" ");
+        console.log($scope.tasks);
     }
 }]).
 controller('CurrencyConverterController', ['$scope', function($scope) {
