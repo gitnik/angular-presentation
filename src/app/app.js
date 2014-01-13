d7f3ec7b291cf507cc334ae945ccba9575ef4cae
Nik Karbaum <nik.karbaum@gmail.com>
2014-01-13 12:51:02 +0100

    fixed typo

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,7 @@
 angular.module('SampleApp', ['SampleApp.factories', 'SampleApp.filters', 'SampleApp.controllers', 'ngRoute']).
 config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/index', { templateUrl: 'partials/index.html', controller: 'NavBarController' });
-    $routeProvider.when('/dummy-link-1', { templateUrl: 'partials/todo.html', controller: 'TodoController' });
+    $routeProvider.when('/dummy-link-1', { templateUrl: 'partials/todo.html', controller: 'ToDoController' });
     $routeProvider.when('/dummy-link-2', { templateUrl: 'partials/currency.html', controller: 'CurrencyConverterController' });
     $routeProvider.otherwise({ redirectTo: '/index' });
 }]);
