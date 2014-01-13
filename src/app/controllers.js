e2419f6567194624c94e4d2c5656831f72476f15
Nik Karbaum <nik.karbaum@gmail.com>
2014-01-13 12:45:27 +0100

    added todo html partial

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -3,7 +3,7 @@ controller('NavBarController', ['$scope', function($scope) {
     $scope.name = "dummy";
 }]).
 controller('ToDoController', ['$scope', function($scope) {
-
+    $scope.tasks = ['Task1', "Task2", "Task3"];
 }]).
 controller('CurrencyConverterController', ['$scope', function($scope) {
 
