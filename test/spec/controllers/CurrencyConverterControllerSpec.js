6806f2c987d6b50fbf356c0dab67542349928a89
Nik K <nik.karbaum@gmail.com>
2014-01-14 19:18:43 +0100

    added test for currencyconvertercontroller. I have the feeling it will fail

---
+++ test/spec/controllers/CurrencyConverterControllerSpec.js
@@ -0,0 +1,16 @@
+describe('CurrencyConverterController', function() {
+    beforeEach(
+        inject(function(CurrencyExchangeData, $q) {
+            var deferred = $q.defer();
+            deferred.resolve('{"to": "EUR", "rate": 0.73236699999999999, "from": "USD"}');
+
+            spyOn(CurrencyExchangeData, 'getData').andReturn(deferred.promise);
+        })
+    );
+
+    it('should have the correct attributes',
+        inject(function(CurrencyConverterController) {
+            expect(CurrencyConverterController.getExchangeRate()).toBe('0.7323');
+        })
+    );
+});
--- test/spec/factories/CurrencyExchangeDataSpec.js
+++ test/spec/factories/CurrencyExchangeDataSpec.js
@@ -1,3 +1,23 @@
-/**
- * Created by Nik on 14.01.14.
- */
+describe('CurrencyExchangeData', function() {
+
+    beforeEach(
+        inject(function(CurrencyExchangeData, $q) {
+            var deferred = $q.defer();
+            deffered.resolve('{"to": "EUR", "rate": 0.73236699999999999, "from": "USD"}');
+
+
+
+        })
+    );
+
+   it('should exist', inject(function(CurrencyExchangeData) {
+       expect(CurrencyExchangeData).toBeDefined();
+   }));
+
+   it('should have a getData() function', inject(function(CurrencyExchangeData){
+       expect(CurrencyExchangeData.getAll()).toBeDefined();
+   }));
+
+   it('should return the json file')
+
+});
