2de472d78029dbf27ef7d2e72a3ccaf41cfb1970
Nik K <nik.karbaum@gmail.com>
2014-01-14 20:25:59 +0100

    five tests. that's more than enough

--- test/spec/controllers/CurrencyConverterControllerSpec.js
+++ test/spec/controllers/CurrencyConverterControllerSpec.js
@@ -3,8 +3,10 @@ describe('CurrencyConverterController', function() {
     var $scope, CurrencyExchangeData, createController;
     beforeEach(
         inject(function($injector, $q) {
-            //var deferred = $q.defer();
-            //deferred.resolve('{"to": "EUR", "rate": 0.73236699999999999, "from": "USD"}');
+
+            var deferred = $q.defer();
+            deferred.resolve({"to": "EUR", "rate": 0.73236699999999999, "from": "USD"});
+
             CurrencyExchangeData = $injector.get('CurrencyExchangeData');
 
             $rootScope = $injector.get('$rootScope');
@@ -18,13 +20,19 @@ describe('CurrencyConverterController', function() {
                 })
             };
 
-            //spyOn(CurrencyExchangeData, 'getData').and.returnValue(deferred.promise);
+            spyOn(CurrencyExchangeData, 'getData').and.returnValue(deferred.promise);
+
         })
     );
 
+    it('should trigger the xhr call', function() {
+        createController();
+        expect(CurrencyExchangeData.getData).toHaveBeenCalled();
+    });
+
     it('should have the correct attributes',  function() {
-        var controller = createController();
-        console.log($scope);
-        expect($scope.getExchangeRate()).toBe('0.7323');
+        createController();
+        $scope.$apply();
+        expect($scope.getExchangeRate()).toBe(0.7324);
     });
 });
--- test/spec/factories/CurrencyExchangeDataSpec.js
+++ test/spec/factories/CurrencyExchangeDataSpec.js
@@ -1,13 +1,38 @@
 describe('CurrencyExchangeData', function() {
 
-   it('should exist', inject(function(CurrencyExchangeData) {
+    var $httpBackend, CurrencyExchangeData;
+
+    beforeEach(
+        inject(function($injector) {
+            $httpBackend = $injector.get('$httpBackend');
+
+            $httpBackend.expectGET("app/exchangeData.json").respond(
+                {"to": "EUR", "rate": 0.73236699999999999, "from": "USD"}
+            );
+
+            CurrencyExchangeData = $injector.get('CurrencyExchangeData');
+        })
+    );
+
+    afterEach(function () {
+        //$httpBackend.verifyNoOutstandingRequest();
+        //$httpBackend.verifyNoOutstandingExpectation();
+    });
+
+   it('should exist', function() {
        expect(CurrencyExchangeData).toBeDefined();
-   }));
+   });
 
-   it('should have a getData() function', inject(function(CurrencyExchangeData){
+   it('should have a getData() function', function(){
        expect(CurrencyExchangeData.getData()).toBeDefined();
-   }));
+   });
 
-   //it('should return the json file')
+   it('should return the correct response', function() {
+       var data;
+       CurrencyExchangeData.getData().then(function(response) {
+           data = response;
 
+           expect(data).toBe({"to": "EUR", "rate": 0.73236699999999999, "from": "USD"});
+       });
+   })
 });
